73db518a79ac40182bc6dc9f610d5a06399aaea8
Noé <eon.sberk@gmail.com>
2023-07-07 13:31:20 +0200

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-//hhello2 with vars
+//hhello2 with vars v2
 app.get("/", (req, res) => {
   res.send(process.env);
 });
